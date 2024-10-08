9a40d71c8f20c7a68638aab636138a5d339b7dd1
Rob Shockency <degnarraer@yahoo.com>
2024-10-08 14:16:57 -0500

    added Audio as a major service for COD

--- src/BluetoothA2DPSource.cpp
+++ src/BluetoothA2DPSource.cpp
@@ -406,9 +406,10 @@ void BluetoothA2DPSource::filter_inquiry_scan_result(
       break;
     }
   }
-  /* search for device with MAJOR service class as "rendering" in COD */
+  /* search for device with MAJOR service class as "rendering" or Audio in COD */
   if (!esp_bt_gap_is_valid_cod(cod) ||
-      !(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_RENDERING)) {
+      !(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_RENDERING) ||
+      !(esp_bt_gap_get_cod_srvc(cod) & ESP_BT_COD_SRVC_AUDIO)) {
     ESP_LOGI(BT_AV_TAG, "--Compatiblity: Incompatible");
     return;
   }
